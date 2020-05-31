2e4fbcac72a66d2c31f341560ab2b252fcf25d75
Pink Moonlight <kate.cherie25@gmail.com>
2020-05-30 22:43:56 -0700

    Updates

---
+++ precache-manifest.f0953420f34526ddd62b37a693dfffd9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff361da94b3beea16e57e686624e50b9",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "db9fff3c39ae26519ae9",
+    "url": "/Pomodoro-Clock/static/css/main.e91a96c9.chunk.css"
+  },
+  {
+    "revision": "b9c1e2bbff52dd891fd2",
+    "url": "/Pomodoro-Clock/static/js/2.1d655426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.1d655426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9fff3c39ae26519ae9",
+    "url": "/Pomodoro-Clock/static/js/main.03d79ad3.chunk.js"
+  },
+  {
+    "revision": "f101a67d633c71df8a02",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.d62db2d2.js"
+  },
+  {
+    "revision": "8e387d865d05590c937e6ab783d5a872",
+    "url": "/Pomodoro-Clock/static/media/beep.8e387d86.mp3"
+  },
+  {
+    "revision": "11c7d98a4da2cb94cb47c6c79bf14cc1",
+    "url": "/Pomodoro-Clock/static/media/coffee.11c7d98a.svg"
+  },
+  {
+    "revision": "f88e183c6fb72f09f6bd3338a907d2ef",
+    "url": "/Pomodoro-Clock/static/media/laptop.f88e183c.svg"
+  },
+  {
+    "revision": "e9530715492483a96d77987ff94ec516",
+    "url": "/Pomodoro-Clock/static/media/loop.e9530715.svg"
+  },
+  {
+    "revision": "810033e53c722c492a1d09169f08357f",
+    "url": "/Pomodoro-Clock/static/media/pause.810033e5.svg"
+  },
+  {
+    "revision": "75a39d29ce0609d657e9cb647f87e487",
+    "url": "/Pomodoro-Clock/static/media/play.75a39d29.svg"
+  }
+]);
